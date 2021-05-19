e94aa1d0ccb70fb9fb2e746e4d324b5cb58a780d
Alec Greenaway <aag3@pdx.edu>
2021-05-18 19:44:55 -0700

    Converted axios.get() calls to fetch()

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,4 @@
-//let axios = require('axios')
-//require("dotenv").config();
-
-//const CRYPTOCOMPAREAPI = process.env.CRYPTOCOMPAREAPI;
+const CRYPTOCOMPAREAPI = "";
 
 // Get the CoinID from crytocompare based on the name or symbol
 // An attempt to find the matching coing Asynchronously
@@ -10,13 +7,11 @@
 //  Or find a way to async search repsonse and end early on success
 async function fetchCryptoCoinId(coinName, coinSymbol) {
   let res = "";
-  try {
-    const response = await axios.get(
-      `https://min-api.cryptocompare.com/data/all/coinlist?summary=true&api_key=${CRYPTOCOMPAREAPI}`
-    );
-
-    await Promise.all(
-      Object.values(response.data.Data).map(async (entry) => {
+  let url = `https://min-api.cryptocompare.com/data/all/coinlist?summary=true&api_key=${CRYPTOCOMPAREAPI}`;
+  let coinId = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      Object.values(data.Data).map((entry) => {
         if (
           entry.Symbol === `${coinSymbol.toUpperCase()}` ||
           entry.FullName.toUpperCase().match(
@@ -25,65 +20,56 @@ async function fetchCryptoCoinId(coinName, coinSymbol) {
         ) {
           res = entry.Id;
         }
-      })
-    );
-    return res;
-  } catch (err) {
-    console.error(err);
-  }
+      });
+      return res;
+    })
+    .catch((error) => console.log(error));
+  return coinId;
 }
 
 async function getTopTenVolume(localCurrency) {
-  try {
-    const response = await axios.get(
-      `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=${localCurrency}&api_key=${CRYPTOCOMPAREAPI}`
-    );
-    let topDict = [];
-    Object.values(response.data.Data).map((entry) => {
-      topDict.push({
-        coinName: entry.CoinInfo.FullName,
-        coinSymbol: entry.CoinInfo.Name,
-        coinPrice: entry.RAW[localCurrency].PRICE,
-        coinVolume: entry.RAW[localCurrency].VOLUMEDAY,
+  let url = `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=${localCurrency}&api_key=${CRYPTOCOMPAREAPI}`;
+  let topTen = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let topDict = [];
+      Object.values(data.Data).map((entry) => {
+        topDict.push({
+          coinName: entry.CoinInfo.FullName,
+          coinSymbol: entry.CoinInfo.Name,
+          coinPrice: entry.RAW[localCurrency].PRICE,
+          coinVolume: entry.RAW[localCurrency].VOLUMEDAY,
+        });
       });
-    });
-    topDict.forEach((entry, index) => {
-      let listItem = document.getElementById(`#item${index + 1}`);
-      listItem.innerHTML = entry.coinSymbol + " -- " + entry.coinName;
-    });
-    return topDict;
-  } catch (err) {
-    console.error(err);
-  }
+      topDict.forEach((entry, index) => {
+        let listItem = document.getElementById(`item${index + 1}`);
+        listItem.innerHTML = entry.coinSymbol + " -- " + entry.coinName;
+      });
+      return topDict;
+    })
+    .catch((error) => console.log(error));
+  return topTen;
 }
 
 // Data from Cryptonator, must use the coin codes outlined here: https://www.cryptonator.com/api/currencies
 // Converts to value type of currency in targetCurrency
 async function getCoinDataToTargetCurrency(currencyCode, targetCurrency) {
-  try {
-    const response = await axios.get(
-      `https://api.cryptonator.com/api/full/${currencyCode}-${targetCurrency}`
-    );
-    return response.data;
-  } catch (err) {
-    console.log(err);
-  }
+  let url = `https://api.cryptonator.com/api/full/${currencyCode}-${targetCurrency}`;
+  let data = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.log(error));
+  return data;
 }
 
 // get social media presence of coin by either coin's name or coin symbol
 // Requires 2 API calls unfortunately, since one needs the coinId integer to use the social media endpoint of API
 async function getCoinSocialMediaActivity(coinName, coinSymbol) {
-  try {
-    let coinId = await fetchCryptoCoinId(coinName, coinSymbol);
-    const response = await axios.get(
-      `https://min-api.cryptocompare.com/data/social/coin/latest?coinId=${coinId}&api_key=${CRYPTOCOMPAREAPI}`
-    );
-    return response.data.Data;
-  } catch (err) {
-    console.log(err);
-  }
+  let coinId = await fetchCryptoCoinId(coinName, coinSymbol);
+  let url = `https://min-api.cryptocompare.com/data/social/coin/latest?coinId=${coinId}&api_key=${CRYPTOCOMPAREAPI}`;
+  let data = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.Data)
+    .catch((error) => console.log(error));
+  return data;
 }
-
-// getCoinSocialMediaActivity("bitcoin", "btc").then((res) => console.log(res));
-// getCoinDataToTargetCurrency("btc", "usd").then((res) => console.log(res));
-// getTopTenVolume("USD").then((res) => console.log(res));
