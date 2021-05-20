e20169d10909842fd5fe0fb844ea4d6e3afdb566
Alec Greenaway <aag3@pdx.edu>
2021-05-20 16:26:34 -0700

    Added styling to top price grid and top volume list

--- scripts/functions.js
+++ scripts/functions.js
@@ -3,17 +3,35 @@ $(document).ready(function () {
   getTopTenVolume("USD").then(function (topTenData) {
     topTenData.sort((a, b) => (a.coinPrice > b.coinPrice ? -1 : 1));
     $(topTenData).each(function (index, value) {
+      let boldSpan = $("<strong></strong>");
+      boldSpan.text(index + 1);
       if (index < 1) {
-        let coinPriceContainer = $("<div></div>").text(value.coinPrice);
+        let coinPriceContainer = $("<div></div>");
+        coinPriceContainer
+          .append(boldSpan)
+          .append(" - ")
+          .append(value.coinPrice);
         $("#price_col_main").append(coinPriceContainer);
       } else if (index < 4) {
-        let coinPriceContainer = $("<div></div>").text(value.coinPrice);
+        let coinPriceContainer = $("<div></div>");
+        coinPriceContainer
+          .append(boldSpan)
+          .append(" - ")
+          .append(value.coinPrice);
         $("#price_col_two").append(coinPriceContainer);
       } else if (index < 7) {
-        let coinPriceContainer = $("<div></div>").text(value.coinPrice);
+        let coinPriceContainer = $("<div></div>");
+        coinPriceContainer
+          .append(boldSpan)
+          .append(" - ")
+          .append(value.coinPrice);
         $("#price_col_three").append(coinPriceContainer);
       } else {
-        let coinPriceContainer = $("<div></div>").text(value.coinPrice);
+        let coinPriceContainer = $("<div></div>");
+        coinPriceContainer
+          .append(boldSpan)
+          .append(" - ")
+          .append(value.coinPrice);
         $("#price_col_four").append(coinPriceContainer);
       }
     });
