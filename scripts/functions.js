80cf4a7b0583bec56bdf67b409bf066d1b42f2aa
Alec Greenaway <aag3@pdx.edu>
2021-05-20 13:52:50 -0700

    added grid for top prices

--- scripts/functions.js
+++ scripts/functions.js
@@ -2,8 +2,19 @@ $(document).ready(function () {
   getNewArticles();
   getTopTenVolume("USD").then(function (topTenData) {
     $(topTenData).each(function (index, value) {
-      let coinPriceContainer = $("<div></div>").text(value.coinPrice);
-      $("#prices_container").append(coinPriceContainer);
+      if (index < 1) {
+        let coinPriceContainer = $("<div></div>").text(value.coinPrice);
+        $("#price_col_main").append(coinPriceContainer);
+      } else if (index < 4) {
+        let coinPriceContainer = $("<div></div>").text(value.coinPrice);
+        $("#price_col_two").append(coinPriceContainer);
+      } else if (index < 7) {
+        let coinPriceContainer = $("<div></div>").text(value.coinPrice);
+        $("#price_col_three").append(coinPriceContainer);
+      } else {
+        let coinPriceContainer = $("<div></div>").text(value.coinPrice);
+        $("#price_col_four").append(coinPriceContainer);
+      }
     });
   });
   $(".articles > div:gt(0)").hide();
