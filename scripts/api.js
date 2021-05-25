a7b80aa55c315ae9bf5b632c9d1fa6b61c0f3171
Ramon <raga2@pdx.edu>
2021-05-25 16:14:20 -0700

    Added default color background for news carasouel

--- scripts/api.js
+++ scripts/api.js
@@ -143,7 +143,7 @@ async function getNewArticles() {
         } else {
           ele.setAttribute("class", "carousel-item");
         }
-        ele.innerHTML = `<img src=" ${entry.image}" class="d-block w-100" alt="..."><div class="carousel-caption d-none d-md-block">
+        ele.innerHTML = `<img src="https://wholistickids.com/wp-content/uploads/2019/01/plain-black-background.jpg" class="d-block w-100" alt="..."><div class="carousel-caption d-none d-md-block">
                          <h5>First slide label</h5>
                          <p>Some representative placeholder content for the first slide.</p>
                          </div>`;
