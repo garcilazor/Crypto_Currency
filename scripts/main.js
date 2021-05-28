3ced701d0ca8a13153396e0bea56fd786d1b06bb
Alec Greenaway <aag3@pdx.edu>
2021-05-28 15:42:30 -0700

    added functionality to compare button to pass parameters to compare page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
   let coinSymbol = null;
+  $("#compare-button").click(function () {
+    let cur1 = $("#compare1").val().toString();
+    let cur2 = $("#compare2").val().toString();
+    let url =
+      "./compare.html" +
+      "?cur1=" +
+      encodeURIComponent(cur1) +
+      "&cur2=" +
+      encodeURIComponent(cur2);
+    window.location.href = url;
+  });
   $("#search-button").click(function () {
     coinSymbol = $("#search-content").val();
     populateHomePageGraphs(coinSymbol);
