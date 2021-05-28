b89472bc78b6dc2db890f4e4f48597a6bbd7eefd
Alec Greenaway <aag3@pdx.edu>
2021-05-28 16:28:14 -0700

    added code to retrive compare parameters from home page in compare.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // We are on home page
   if ($("body").attr("id") === "home") {
     let coinSymbol = null;
     $("#compare-button").click(function () {
@@ -20,10 +21,20 @@ $(document).ready(function () {
     populateHomePageGraphs(coinSymbol);
     getMiningData("BTC", "ETH", "USD");
   }
+  // We are on compare page
   if ($("body").attr("id") === "compare") {
     MutlipleSymbolsFullData("BTC", "ETH", "USD").then((res) => {
       MultiSymbolFullDataChart(res);
     });
+    let searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has("cur1") && searchParams.has("cur2")) {
+      // Parameters were passed in
+      let cur1 = searchParams.get("cur1");
+      let cur2 = searchParams.get("cur2");
+      console.log(cur1, cur2);
+    } else {
+      // No parameters, populate with default values
+    }
     // HistoricalDailyBlockChain(coinSymbol).then((res) => {
     //   blockChainLineGraph(coinSymbol, res);
     // });
